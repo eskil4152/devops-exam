ee7d17b690f4d153b77cec54848354a301c27ba8
eskil <eskiltb@hotmail.com>
2022-11-28 21:34:06 +0100

    Alarm added

---
+++ infra/alarms.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_metric_alarm" "many_carts" {
+  alarm_name          = "Threshold for over 5 carts"
+  namespace           = var.candidate_id
+  metric_name         = "carts.value"
+
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "5"
+  evaluation_periods  = "3"
+  period = "300"
+
+  statistic = "Maximum"
+
+  alarm_description = "Alarm goes off 5+ carts are registered in a 5 minute period 3 times"
+  insufficient_data_actions = []
+  alarm_actions = [aws_sns_topic.alarms.arn]
+}
