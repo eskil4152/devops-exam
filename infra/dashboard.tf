e84cdb315dd501f562a34d50bd8d6cee70cb99d0
eskil <eskiltb@hotmail.com>
2022-12-01 15:59:48 +0100

    .count?

--- infra/dashboard.tf
+++ infra/dashboard.tf
@@ -33,7 +33,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.candidate_id}",
-            "checkouts.value"
+            "checkouts.count"
           ]
         ],
         "period": 60,
